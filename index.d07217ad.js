d76576124f9d64e3bbbf902bccbcebf6d0b1a26a
Oleh Chernov <nineuito@mail.com>
2024-06-15 14:32:33 +0300

    make build

---
+++ index.d07217ad.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",function(){let e=document.querySelector(".top-bar-header");window.scrollY>0?e.classList.add("scrolled"):e.classList.remove("scrolled")}),setInterval(function(){let e=document.querySelector(".page__body");"rgb(255, 238, 252)"===getComputedStyle(e).getPropertyValue("background-color").trim()?e.style.backgroundColor="#EEF7FF":e.style.backgroundColor="#FFEEFC"},5e3);
+//# sourceMappingURL=index.d07217ad.js.map
